4a1aaac2620e70fab8d8ad143adc463dceb21247
Daniel Benediktsson <danielb13@ru.is>
2015-10-05 15:32:56 +0000

    flot

--- post.sh
+++ post.sh
@@ -1,2 +1,2 @@
 #/bin/bash
-curl -d "this is post content, i.e paylod in http package" localhost:$(/labs/tsam/my_port)
+curl -d "this is post content, i.e paylod in http package" localhost:$(/labs/tsam15/my_port)
